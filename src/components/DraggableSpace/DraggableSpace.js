1acc48e36ddcd3fbb2d54d04adabdba02a124dab
Brea Stuns <breastuns@gmail.com>
2019-10-25 00:48:59 -0700

    Render multiple DraggableItems with unique ids

--- src/components/DraggableSpace/DraggableSpace.js
+++ src/components/DraggableSpace/DraggableSpace.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { DraggableItem } from '../DraggableItem/DraggableItem'
+import { DraggableItem } from '../DraggableItem/DraggableItem';
+import { ThemeToggle } from '../ThemeToggle/ThemeToggle';
 import './DraggableSpace.scss';
 
-export function DraggableSpace () {
-    return (
-        <div className="draggable-space">
-            <DraggableItem type={'circle'}/>
-            <DraggableItem type={'box'}/>
-        </div>
-    );
+export function DraggableSpace() {
+  let boxElements = [];
+  let circleElements = [];
+  for (let i = 0; i <= 10; i++) {
+    boxElements.push(<DraggableItem type={'box'} id={'box' + i} key={'box' + i} />);
+    circleElements.push(<DraggableItem type={'circle'} id={'circle' + i} key={'circle' + i} />);
+  }
+
+  return (
+    <div className="draggable-space">
+      <ThemeToggle />
+      {boxElements}
+      {circleElements}
+    </div>
+  );
 }
